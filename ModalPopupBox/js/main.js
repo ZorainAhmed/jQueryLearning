c44b0be976da54b2719a9ca4e57922355eb3518c
Zorain <izorainahmed@gmail.com>
2022-03-09 13:06:39 +0500

    Second Commit

---
+++ ModalPopupBox/js/main.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+   $(".subscribe-btn").on("click",function () {
+     $("#popup").addClass("active"); 
+   });
+   $(".close").on("click",function () {
+    $("#popup").removeClass("active"); 
+  });
+});
---
+++ StickyNavbar/js/main.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){
+    $(window).on("scroll",function() {
+        if($(this).scrollTop() > 80){
+            $("header").addClass("sticky");
+        } else {
+            $("header").removeClass("sticky");
+        }
+    });
+});
