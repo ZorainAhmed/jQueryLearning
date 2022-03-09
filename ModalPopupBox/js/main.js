343c1281ce8273a3bbac1e7863ec45163d48e3dd
Zorain <izorainahmed@gmail.com>
2022-03-09 16:55:06 +0500

    Third Commit

--- ModalPopupBox/js/main.js
+++ ModalPopupBox/js/main.js
@@ -1,8 +1,8 @@
 $(document).ready(function () {
-   $(".subscribe-btn").on("click",function () {
-     $("#popup").addClass("active"); 
-   });
-   $(".close").on("click",function () {
+  $(".subscribe-btn").on("click",function () {
+    $("#popup").addClass("active"); 
+  });
+  $(".close").on("click",function () {
     $("#popup").removeClass("active"); 
   });
 });
